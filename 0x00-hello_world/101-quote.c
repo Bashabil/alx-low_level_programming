1427f1e36ffa055b9f1dc55cfe1203dbe0af0594
Abubakar <talk2asquare@gmail.com>
2022-03-11 15:44:54 -0800

    Quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Kopar, 2015-10-19");
+return (1);
+}
