9c805ee0dc4b90d448ce072cc49feba6a86d4b5a
Abubakar <talk2asquare@gmail.com>
2022-03-12 14:35:40 -0800

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,24 @@ int main(void)
 {
 int x = 0, y = 1;
 while (x <= 8)
-{
-for (; y <= 9; y++)
-{
-putchar(x + '0');
-putchar(y + '0');
-if (y < 9)
-{
-putchar(',');
-}
-else
-{
-continue;
-}
-putchar(' ');
-}
-x++;
-y = y + 1;
-}
+  {
+    for (; y <= 9; y++)
+      {
+	putchar(x + '0');
+	putchar(y + '0');
+	if (y < 9)
+	  {
+	    putchar(',');
+	  }
+	else
+	  {
+	    continue;
+	  }
+	putchar(' ');
+      }
+    x++;
+    y = y + 1;
+  }
 putchar('\n');
 return (0);
 }
