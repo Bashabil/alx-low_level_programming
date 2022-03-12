8209fae8483a1f1b87780be4aded65b165855f8a
Abubakar <talk2asquare@gmail.com>
2022-03-12 12:05:57 -0800

    update to print combination syntax

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,13 @@ int main(void)
 int digit;
 for (digit = 0; digit <= 9; digit++)
 {
-putchar(digit);
+if (digit == 9)
+{
+putchar(digit + '0');
+}
+else
+{
+putchar(digit + '0');
 putchar(',');
 putchar(' ');
 }
