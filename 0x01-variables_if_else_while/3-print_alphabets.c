aa99c50b15008b580387eb3fa9032df5b83b6007
Abubakar <talk2asquare@gmail.com>
2022-03-11 19:21:31 -0800

    Print alphabets in upper and lower case together

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 'a';
+int y = 'A';
+while (x <= 'z')
+{
+putchar(x);
+}
+while (y <= 'Z')
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
