d263198a0624980d05375b6232a698d17b38a84d
Abubakar <talk2asquare@gmail.com>
2022-03-15 20:06:35 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,20 +3,20 @@
 #include "main.h"
 /**
  * _islower - entry point
- * Description: it prints alphabets in lowercase
- * followed by a new line and prototype void print_aphabet(void
+ * Description: it checks if the parameter entered
+ * is in lower case using prototype int _islower(int c
  * Return: Always 0 (Success)
  */
 int _islower(int c) /* This is the function used */
 {
-  int x;
-  x = islower(c);
-  if (x > 0)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+int x;
+x = islower(c);
+if (x > 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
