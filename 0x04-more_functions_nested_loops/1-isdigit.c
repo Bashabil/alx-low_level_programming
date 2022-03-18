c9aad996d6d448f858f48b3f38804cbd204b7d71
Abubakar <talk2asquare@gmail.com>
2022-03-17 17:53:47 -0700

    Check if the number entered is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - Entry Point
+ * Description: Check if the number entered is a digit
+ * @c: number
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c) /* This is where the function begins */
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
