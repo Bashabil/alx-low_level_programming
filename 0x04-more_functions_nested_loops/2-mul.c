6c59831e3319ca176740bfd0683f1baad166c5b7
Abubakar <talk2asquare@gmail.com>
2022-03-17 18:16:23 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - Entry Point
+ * Description: Check if the number entered is a digit
+ * @a: number
+ * @b: number
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b) /* This is where the function begins */
+{
+int x = a * b;
+_putchar(x + '0');
+return (0);
+}
