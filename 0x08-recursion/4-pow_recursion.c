4ec1a085aa9a05bc45f84074e9a1f5005ac0b29a
MAC <mac@ozoza.local>
2022-03-31 05:02:39 +0100

    Task 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Power Recursion
+ * @x: Interger paramater
+ * @y: Interger parameter
+ * Return: recursion
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
