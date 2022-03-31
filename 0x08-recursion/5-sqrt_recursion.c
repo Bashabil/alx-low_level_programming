ed7469fc24eba813f60faf6c010a409fdb451982
MAC <mac@ozoza.local>
2022-03-31 05:09:14 +0100

    Task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Square Root Recursion
+ * @n: integer params
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer params
+ * @i: integer params
+ * Return: sqrt
+ */
+
+int _sqrt(int n, int i)
+{
+if (n < 0)
+{
+return (-1);
+}
+if ((i * i) > n)
+{
+return (-1);
+}
+if (i * i == n)
+{
+return (i);
+}
+return (_sqrt(n, i + 1));
+}
